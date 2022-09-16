c7936d83663f7bcb76ff094e254868b524c06a58
Abhishek Sokhal <abhisheksokhal2035@gmail.com>
2022-09-16 18:53:03 +0530

    main commit

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ChangeColor : MonoBehaviour
+{
+    public void ChangeColorEnter()
+    {
+        GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    }
+
+    public void ChangeColorExit()
+    {
+        GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    }
+}
